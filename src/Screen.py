4a24dcced2a8af175d4398d528d5354b986ec3ff
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-04-02 14:53:37 -0700

    Update Screen.py

--- src/Screen.py
+++ src/Screen.py
@@ -42,10 +42,7 @@ class Drawable:
     def __init__(self, img, x, y, center=False): # x, y: center or (left, top) coordinates
         width  = img.get_width()
         height = img.get_height()
-        if center:
-            origin = (x - (width / 2), y - (height / 2))
-        else:
-            origin = (x, y)
+        origin = (x - (wdith // 2), y - (height // 2)) if center else (x, y)
     
         self.img  = img
         self.rect = pygame.Rect(origin, (width, height))
