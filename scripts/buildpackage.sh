7e1aebdef202f56dd28dd8d50f79378c14d2712a
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-04-03 22:04:58 -0700

    Linux scripts

--- scripts/buildpackage.sh
+++ scripts/buildpackage.sh
@@ -1,9 +1,12 @@
 
-mkdir -sgew package/SpaceRun
 executable="pyinstaller/dist/SpaceRun"
 package="package/SpaceRun"
 
-if $executable exists:
-    rm contents of $package
-    cpy $executable $package
-    cpy $resources $package
+mkdir -p $package
+
+if [[ -f $executable ]]; then
+    cp -f $executable $package
+    cp -f -r resources $package
+else
+    echo "No executable found."
+fi
