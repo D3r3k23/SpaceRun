90daa788d7d216be5125c5e78bda6c5e113549f7
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-07-26 23:48:44 -0700

    Updated Music to use listdirs

--- src/Resources.py
+++ src/Resources.py
@@ -1,7 +1,7 @@
 
 from Music import Music
 
-import os.path
+import os
 import pygame
 
 resource_dir = 'resources'
@@ -101,16 +101,8 @@ def load_sounds():
     pygame.mixer.set_reserved(1)
 
 def load_music():
-    music.add_song(get_song_path('The Prototypes - Pale Blue Dot.ogg'))
-    music.add_song(get_song_path('Teddy Killerz - Outer Space.ogg'))
-    music.add_song(get_song_path('Phace & Misanthrop - Desert Orgy.ogg'))
-    music.add_song(get_song_path('Mefjus & Break - Out Of Time.ogg'))
-    music.add_song(get_song_path('Current Value - Dark Rain.ogg'))
-    music.add_song(get_song_path('Noisia - Facade.ogg'))
-    music.add_song(get_song_path('Fortran - Alien Girl.ogg'))
-    music.add_song(get_song_path('Ed Rush & Optical - Mystery Machine.ogg'))
-    music.add_song(get_song_path('Ed Rush & Optical - Fixation.ogg'))
-    music.add_song(get_song_path('Andy C - Quest (Bladerunner Remix).ogg'))
+    for song in os.listdir(os.path.join('resources', 'music')):
+        music.add_song(get_song_path(song))
 
 def load_fonts():
     fonts['SpaceSquadron'] = get_font_path('space_squadron.ttf')
